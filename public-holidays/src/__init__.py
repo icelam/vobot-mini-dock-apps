71d013ef392b45c48646573de636af0eaca48dbd
Ice Lam <pinky.yy.lam@gmail.com>
2024-12-24 17:55:09 +0800

    feat(public-holidays): add holiday contdown

--- public-holidays/src/__init__.py
+++ public-holidays/src/__init__.py
@@ -1,5 +1,6 @@
 import lvgl as lv
 import clocktime
+import utime
 import ujson
 import net
 import urequests
@@ -34,7 +35,7 @@ previous_focus_index = -1
 def reset_style(style_object):
     style_object.set_bg_opa(lv.OPA.COVER)
     style_object.set_bg_color(lv.color_hex(0xFFFFFF))
-    style_object.set_text_color(lv.color_hex(0x2D3436))
+    style_object.set_text_color(lv.color_hex(0x454545))
     style_object.set_border_width(0)
     style_object.set_pad_all(0)
     style_object.set_radius(0)
@@ -46,7 +47,7 @@ header_style = lv.style_t()
 header_style.init()
 reset_style(header_style)
 header_style.set_bg_color(lv.color_hex(0xFDCB6E))
-header_style.set_text_color(lv.color_hex(0x2D3436))
+header_style.set_text_color(lv.color_hex(0x454545))
 header_style.set_pad_all(12)
 
 # List style
@@ -54,11 +55,18 @@ list_style = lv.style_t()
 list_style.init()
 reset_style(list_style)
 
-# item style
+# Container style
+container_style = lv.style_t()
+container_style.init()
+reset_style(container_style)
+container_style.set_bg_opa(lv.OPA.TRANSP)
+
+# Item style
 item_style = lv.style_t()
 item_style.init()
 reset_style(item_style)
-item_style.set_pad_all(10)
+item_style.set_pad_ver(5)
+item_style.set_pad_hor(10)
 item_style.set_border_color(lv.color_hex(0xEEEEEE))
 item_style.set_border_width(2)
 item_style.set_border_side(lv.BORDER_SIDE.BOTTOM)
@@ -67,6 +75,25 @@ item_style.set_border_side(lv.BORDER_SIDE.BOTTOM)
 focused_item_style = lv.style_t()
 focused_item_style.init()
 focused_item_style.set_bg_color(lv.color_hex(0xf0f0f0))
+
+# Chips style
+chip_style = lv.style_t()
+chip_style.init()
+reset_style(chip_style)
+chip_style.set_radius(5)
+chip_style.set_pad_all(10)
+chip_style.set_text_align(lv.TEXT_ALIGN.CENTER)
+chip_style.set_bg_color(lv.color_hex(0x000000))
+chip_style.set_text_color(lv.color_hex(0xffffff))
+
+# Remarks style
+remarks_style = lv.style_t()
+remarks_style.init()
+reset_style(remarks_style)
+remarks_style.set_pad_ver(2)
+remarks_style.set_bg_opa(lv.OPA.TRANSP)
+remarks_style.set_text_color(lv.color_hex(0x999999))
+remarks_style.set_text_font(lv.font_ascii_14)
 
 # ---------- Functions ----------
 def dprint(msg: str) -> None:
@@ -202,7 +229,12 @@ def focus_item(index: int) -> None:
     previous_focus_index = index
 
 def get_current_date() -> int:
-    """Get and return current date as YYYYMMDD format"""
+    """
+    Get and return current date as YYYYMMDD format
+
+    Returns:
+        int: Current date in YYYYMMDD format.
+    """
     current_time = clocktime.datetime()
     year = current_time[0]
     month = current_time[1]
@@ -210,6 +242,12 @@ def get_current_date() -> int:
 
     formatted_date = '{:04d}{:02d}{:02d}'.format(year, month, day)
     return int(formatted_date)
+
+def days_between(date1: int, date2: int):
+    time_info = (0, 0, 0, 0, 0)
+    datetime1 = (date1 // 10000, date1 % 10000 // 100, date1 % 100) + time_info
+    datetime2 = (date2 // 10000, date2 % 10000 // 100, date2 % 100) + time_info
+    return abs(utime.mktime(datetime1) // (24 * 3600) - utime.mktime(datetime2) // (24 * 3600))
 
 def fetch_and_display_public_holiday(current_date: int) -> None:
     """
@@ -232,7 +270,6 @@ def fetch_and_display_public_holiday(current_date: int) -> None:
     header.update_layout()
 
     container_height = SCREEN_HEIGHT - header.get_height();
-    print(container_height)
 
     # Add list container
     list_container = lv.list(main_scr)
@@ -254,23 +291,39 @@ def fetch_and_display_public_holiday(current_date: int) -> None:
         item = lv.obj(list_container)
         item.add_style(item_style, 0)
         item.add_style(focused_item_style, lv.STATE.FOCUSED)
-        item.set_height(50)
+        item.set_size(SCREEN_WIDTH, container_height // 3)
 
-        name_label = lv.label(item)
+        left_content = lv.obj(item)
+        left_content.add_style(container_style, 0)
+        left_content.align(lv.ALIGN.LEFT_MID, 0, 0)
+        left_content.set_size(140, (container_height // 3) - 12);
+
+        name_label = lv.label(left_content)
         name_label.set_text(event['summary'])
         name_label.set_long_mode(lv.label.LONG.SCROLL_CIRCULAR)
-        name_label.set_width(150)
-        name_label.align(lv.ALIGN.LEFT_MID, 0, 0)
+        name_label.set_style_pad_ver(5, 0)
+        name_label.set_width(140)
+        name_label.align(lv.ALIGN.TOP_LEFT, 0, 0)
 
-        date_label = lv.label(item)
+        date_label = lv.label(left_content)
+        date_label.add_style(remarks_style, 0)
         date_str = event['dtstart'][0]
         date_label.set_text(f"{date_str[:4]}-{date_str[4:6]}-{date_str[6:]}")
         date_label.set_long_mode(lv.label.LONG.SCROLL_CIRCULAR)
-        date_label.set_width(110)
-        date_label.align(lv.ALIGN.RIGHT_MID, 0, 0)
-        date_label.set_style_text_align(lv.TEXT_ALIGN.RIGHT, 0)
+        date_label.set_width(140)
+        date_label.align(lv.ALIGN.BOTTOM_LEFT, 0, 0)
 
-        holiday_count = holiday_count + 1
+        countdown_chip = lv.label(item)
+        countdown_chip.add_style(chip_style, 0)
+        holiday_date = int(event['dtstart'][0])
+        countdown = days_between(current_date, holiday_date)
+        countdown_chip.set_text(f"還有 {countdown} 天" if countdown > 0 else "今天")
+        countdown_chip.set_style_bg_color(lv.color_hex(0x4C89B2) if countdown > 0 else lv.color_hex(0xE25E55), 0)
+        countdown_chip.set_long_mode(lv.label.LONG.SCROLL_CIRCULAR)
+        countdown_chip.set_width(140)
+        countdown_chip.align(lv.ALIGN.RIGHT_MID, 0, 0)
+
+        holiday_count = len(future_events)
 
     lv.screen_load(main_scr)
 
@@ -292,7 +345,6 @@ async def on_start():
         last_api_call_date = current_date
         fetch_and_display_public_holiday(current_date)
     except Exception as e:
-        print(e)
         display_error_screen(f"Error occured on start: {e}")
 
 async def on_running_foreground():
